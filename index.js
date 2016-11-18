22d321a5f06be4e1854cc4713ef7108736fb0292
tunnckoCore <mameto_100@mail.bg>
2016-11-18 23:16:45 +0200

    chore(init): push

---
+++ index.js
@@ -0,0 +1,8 @@
+/*!
+ * kind-of-extra <https://github.com/tunnckoCore/kind-of-extra>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
---
+++ test.js
@@ -0,0 +1,16 @@
+/*!
+ * kind-of-extra <https://github.com/tunnckoCore/kind-of-extra>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+var test = require('mukla')
+
+test(function (done) {
+  done()
+})
