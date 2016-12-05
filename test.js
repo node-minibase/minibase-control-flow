d883a564be1f8b3ee7a6b394e9751b71037323cb
tunnckoCore <mameto_100@mail.bg>
2016-12-05 14:32:07 +0200

    fix(update): few tweaks

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * minibase-control-flow <https://github.com/node-minibase/minibase-control-flow>
  *
- * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://i.am.charlike.online)
  * Released under the MIT license.
  */
 
